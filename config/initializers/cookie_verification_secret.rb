1672581775a340f25d1016e1b26456d1848b8656
Dr Nic Williams <drnicwilliams@gmail.com>
2010-06-05 09:57:22 -0700

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '948d34626d15ad1802ea84ca9ab59fa56d6f83967c048fd3277d48cfb2bd7cb99f32f223dda1b0fde7ef97ec4f0b68db97310c0f82c35156e8665a67f3a00db6';
---
+++ lib/tasks/cron.rake
@@ -0,0 +1,4 @@
+desc "Entry point for cron tasks"
+task :cron do
+
+end
