17f655be2175bb75659ba7732871fa3e6d3d5ee7
Dr Nic Williams <drnicwilliams@gmail.com>
2010-06-27 14:28:46 +1000

    updating the beforedeploy for bundler latest commands

--- deploy/before_deploy.rb
+++ deploy/before_deploy.rb
@@ -1,2 +1,2 @@
 # From https://community.engineyard.com/discussions/questions/409-does-bundler-play-nice-with-cloud-deploy
-run "cd #{release_path} && gem bundle"
+run "cd #{release_path} && bundle install && bundle lock"
